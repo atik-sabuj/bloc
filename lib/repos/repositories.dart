c64e09f3801871612fcb3d72ed20fa161cc34b48
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-02-08 00:18:36 +0600

    repository created

---
+++ lib/repos/repositories.dart
@@ -0,0 +1,17 @@
+
+import 'dart:convert';
+import 'package:http/http.dart';
+
+class UserRepository {
+  String endpoint = 'https://reqres.in/api/user?page=2';
+  getUser() async {
+    Response response = await get(Uri.parse(endpoint));
+    if(response.statusCode == 200){
+      final List result = jsonDecode(response.body)['data'];
+      return result.map((e) => UserModel.fromJson(e))).toList();
+
+    }else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+}
